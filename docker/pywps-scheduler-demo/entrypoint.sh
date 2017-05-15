cdedc4615540217fa0fd8f7f70a0161bacbf16a8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-15 11:10:40 +0200

    cleaned up start script

--- docker/pywps-scheduler-demo/entrypoint.sh
+++ docker/pywps-scheduler-demo/entrypoint.sh
@@ -11,11 +11,10 @@
 set -o allexport
 # update the wps config (customized by environment variables)
 make update-config
-# start supervisor service with wps and nginx
-make start
 # update ssh known_hosts
-echo "updating ssh known_hosts. please wait ..."
-sleep 10
+echo "Updating ssh known_hosts. Please wait ..."
+sleep 5  # sleep 5 secs
 ssh-keyscan slurm > /home/demo/.ssh/known_hosts
 chown www-data /home/demo/.ssh/known_hosts
-echo "done."
+# start supervisor service with wps and nginx
+make start
