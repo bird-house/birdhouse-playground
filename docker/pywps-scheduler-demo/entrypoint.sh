1207de2d3ee4ab32ab0e2ea4b3588f9c01eca9f8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-23 15:22:39 +0200

    added drmaa package

--- docker/pywps-scheduler-demo/entrypoint.sh
+++ docker/pywps-scheduler-demo/entrypoint.sh
@@ -11,10 +11,5 @@
 set -o allexport
 # update the wps config (customized by environment variables)
 make update-config
-# update ssh known_hosts
-echo "Updating ssh known_hosts. Please wait ..."
-sleep 5  # sleep 5 secs
-ssh-keyscan slurm > /home/demo/.ssh/known_hosts
-chown www-data /home/demo/.ssh/known_hosts
 # start supervisor service with wps and nginx
 make start
