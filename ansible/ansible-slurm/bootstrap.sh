7c905eb69440ac89945ada93360b6b6277354c52
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-07-24 13:51:54 +0200

    working on centos install

--- ansible/ansible-slurm/bootstrap.sh
+++ ansible/ansible-slurm/bootstrap.sh
@@ -9,6 +9,10 @@ if [ -f /etc/debian_version ] ; then
   sudo apt-get install -y ansible
 elif [ -f /etc/redhat-release ] ; then
   echo "Install CentOS packages for Ansible ..."
+  sudo yum update -y
+  # ansible is in EPEL
+  sudo yum install -y epel-release
+  sudo yum install -y ansible
 fi
 
 echo "Bootstrapping done"
