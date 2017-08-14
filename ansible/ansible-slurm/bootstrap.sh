2752fc5ad2ebd9d50292890dd6c31316a3b684ee
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-14 17:22:51 +0200

    fixed hosts

--- ansible/ansible-slurm/bootstrap.sh
+++ ansible/ansible-slurm/bootstrap.sh
@@ -3,6 +3,7 @@ echo "Bootstrapping ..."
 
 if [ -f /etc/debian_version ] ; then
   echo "Install Debian/Ubuntu packages for Ansible ..."
+  sudo apt-get update -y
   sudo apt-get install -y software-properties-common
   sudo apt-add-repository -y ppa:ansible/ansible
   sudo apt-get update -y
