0c4e12c7d10bddb28cf0ac4310b93eba09fcd3c9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-18 14:49:33 +0200

    fixed bootstrap

--- ansible/ansible-slurm/bootstrap.sh
+++ ansible/ansible-slurm/bootstrap.sh
@@ -5,7 +5,9 @@ if [ -f /etc/debian_version ] ; then
   echo "Install Debian/Ubuntu packages for Ansible ..."
   sudo apt-get update -y
   sudo apt-get install -y software-properties-common
-  sudo apt-add-repository -y ppa:ansible/ansible
+  # sudo apt-add-repository -y ppa:ansible/ansible
+  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   sudo apt-get update -y
   sudo apt-get install -y ansible
 elif [ -f /etc/redhat-release ] ; then
